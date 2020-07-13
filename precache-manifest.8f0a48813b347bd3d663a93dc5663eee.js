a80245d982f6e7f99c47850f658cb9fbc785ad78
jeonbyunghoon <wjsqudgns92@gmail.com>
2020-07-13 16:15:57 +0900

    Updates

---
+++ precache-manifest.8f0a48813b347bd3d663a93dc5663eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867090e1241191a3ad74a9c8c3b9454e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c185e325592c45c6dfb6",
+    "url": "/movie_app_2020/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "21743db7752266b68615",
+    "url": "/movie_app_2020/static/js/2.7067e2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.7067e2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c185e325592c45c6dfb6",
+    "url": "/movie_app_2020/static/js/main.e39facfc.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
